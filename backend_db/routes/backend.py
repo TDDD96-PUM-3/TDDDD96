beaffa62569767dda4f987f798aaa1d9d80c805b
Oscar Angantyr <oscar.angantyr@gmail.com>
2026-04-15 14:39:16 +0200

    refactor: merged backend into database folder

--- backend_db/routes/backend.py
+++ backend_db/routes/backend.py
@@ -1,21 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Blueprint, jsonify, request
 from universal_scraper import get_scraping_data
 
-
-app = Flask(__name__)
-
-# placeholder example route call
+backend_bp = Blueprint('backend', __name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-# Route to recive frontend call, may change and further or differnt
-# implementation may be needed. Currently uses args to get the url.
-
-
-@app.route('/scrape_url', methods=['POST'])
+@backend_bp.route('/scrape', methods=['GET'])
 def scrape_url():
     """ Endpoint to receive a URL from the frontend, scrape it, and return the data as JSON."""
     url = request.args.get('url', type=str)
@@ -26,11 +15,3 @@ def scrape_url():
     if data is None:
         return jsonify({'error': 'Failed to scrape the URL'}), 400
     return jsonify(data), 200
-
-
-def run_app():
-    app.run(debug=True)
-
-
-if __name__ == '__main__':
-    run_app()
--- database/routes/__init__.py
+++
@@ -1,2 +0,0 @@
-from .auth import auth_bp
-from .saved_data import data_bp
